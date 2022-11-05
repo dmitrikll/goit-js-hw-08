77f6268f18e4b1d38ce628d7dca5f6c4be332486
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-11-05 16:36:03 +0200

    add task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-console.log(Player);
+// console.log(Player);
+
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const memoryTime = localStorage.getItem(PLAYER_CURRENT_TIME);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe); 
+
+if (memoryTime) {
+    player.setCurrentTime(memoryTime);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000))
+
+function onTimeUpdate(event) { 
+    const time = event.seconds;
+    localStorage.setItem(PLAYER_CURRENT_TIME, time);
+};

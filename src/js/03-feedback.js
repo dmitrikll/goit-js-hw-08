06874fa9644b0a94a23dd04119ff6ab6340fbc74
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-11-05 23:35:44 +0200

    add task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const feedbackFormData = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let memoryText = {};
+
+addUserData();
+
+feedbackFormData.addEventListener('input', throttle(onTextInput, 500));
+feedbackFormData.addEventListener('submit', onFormSubmit);
+
+function onTextInput(event) {
+    memoryText = localStorage.getItem(STORAGE_KEY);
+    memoryText = JSON.parse(memoryText) || {};
+    memoryText[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(memoryText));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    console.log(memoryText);
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function addUserData() {
+    const saveText = localStorage.getItem(STORAGE_KEY);
+    if (saveText) {
+        const parsSaveText = JSON.parse(saveText)
+        Object.entries(parsSaveText).forEach(([name, value]) => {
+            feedbackFormData.elements[name].value = value;
+        });
+    };
+};

cc9eeb936bd048c411125e9a769ad93251e0c827
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-11-05 11:50:38 +0200

    add libreries

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
